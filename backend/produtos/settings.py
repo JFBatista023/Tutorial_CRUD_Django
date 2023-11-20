53cf7f88d6ad0bb888e2cedb99f5114cb31edb19
João Filipe <joaofilipeb045@gmail.com>
2023-11-19 23:25:00 -0300

    Configurando model e media

--- backend/produtos/settings.py
+++ backend/produtos/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = "produtos_imagens/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "produtos_imagens")
+MEDIA_ROOT = BASE_DIR / "produtos_imagens/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
